3ad7e0f49236843dc29208ddc9369a0994510c7f
Lipton Barua <liptonbarua274@gmail.com>
2022-10-11 11:00:18 +0600

    project-57

--- src/Components/Topies/Topies.js
+++ src/Components/Topies/Topies.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import Topie from '../Card/Topie';
+
 
 const Topies = () => {
     const projects = useLoaderData()
     return (
         <div>
            <h1>{projects.data.length}</h1>
+           {
+            projects.map((project)=>console.log(project))
+           }
         </div>
     );
 };
