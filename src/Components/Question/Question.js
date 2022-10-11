d437ace7e84260b02f025d901060f833436452f5
Lipton Barua <liptonbarua274@gmail.com>
2022-10-11 23:24:59 +0600

    project-19

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -4,25 +4,25 @@ const Question = ({ans}) => {
     const{id,question, options}=ans;
     return (
         <div className='container bg-light p-10 my-10 shadow-lg'>
-            <h1>{question}</h1>
+            <h1 className='text-justify'>{question}</h1>
             <from class="grid grid-cols-1 md:grid-cols-2 gap-4">
              <div className='bg-black text-white p-5 text-center flex items-center'>
-             <input type="radio" id={options[0]} name="contact" value="email" checked />
-             <label for={options[0]}>{options[0]}</label>
+             
+             <label for={options[0]}><input type="radio" id={options[0]} name="contact" value="email" checked />{options[0]}</label>
              </div>
 
          <div className='bg-black text-white p-5 text-center flex items-center'>
-         <input type="radio" id={options[1]} name="contact" value="email" checked />
-             <label for={options[1]}>{options[1]}</label>
+         
+             <label for={options[1]}><input type="radio" id={options[1]} name="contact" value="email" checked />{options[1]}</label>
          </div>
         <div className='bg-black text-white p-5 text-center flex items-center'>
-             <input type="radio" id={options[2]} name="contact" value="email" checked />
-             <label for={options[2]}>{options[2]}</label>
+            
+             <label for={options[2]}> <input type="radio" id={options[2]} name="contact" value="email" checked />{options[2]}</label>
         </div>
 
        <div className='bg-black text-white p-5 text-center flex items-center'>
-             <input type="radio" id={options[3]} name="contact" value="email" checked />
-             <label for={options[3]}>{options[3]}</label>
+             
+             <label for={options[3]}><input type="radio" id={options[3]} name="contact" value="email" checked />{options[3]}</label>
        </div>
 </from>
         </div>
