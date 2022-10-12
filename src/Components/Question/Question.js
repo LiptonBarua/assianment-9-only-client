9a89d84b8a5a828f636e6f1d3f259300b3229957
Lipton Barua <liptonbarua274@gmail.com>
2022-10-12 12:48:57 +0600

    project-21

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,43 +1,56 @@
 import React from 'react';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEye } from '@fortawesome/free-solid-svg-icons'
 const Question = ({ans}) => {
-    const{id,question, options,correctAnswer}=ans;
+    const{question, options,correctAnswer}=ans;
     console.log(options)
     
-    // console.log(correctAnswer.length===options[0].length);
 const handclick=(id)=>{
-    // console.log(correctAnswer)
   if(correctAnswer===id){
-   alert (true)
+    toast.success("True !", {
+        autoClose: 2000,
+      });
   }
   else {
-    alert (false)
+    toast.error("False.", {
+        autoClose: 2000,
+      });
   }
 }
 
     return (
+        <div>
+           
+       
         <div className='container bg-light p-10 my-10 shadow-lg'>
+         <FontAwesomeIcon className='ml-[95%]' icon={faEye} />
+            
             <h1 className='text-justify'>{question}</h1>
             <from class="grid grid-cols-1 md:grid-cols-2 gap-4">
              <div className='bg-black text-white p-5 text-center flex items-center'>
+             <ToastContainer />
              
              <label onClick={()=>handclick(options[0])} for={options[0]}><input type="radio" id={options[0]} name="contact" value="email" checked />{options[0]}</label>
              </div>
 
          <div className='bg-black text-white p-5 text-center flex items-center'>
-         
+         <ToastContainer />
              <label onClick={()=>handclick(options[1])} for={options[1]}><input type="radio" id={options[1]} name="contact" value="email" checked />{options[1]}</label>
          </div>
         <div className='bg-black text-white p-5 text-center flex items-center'>
-            
+        <ToastContainer />
+        
              <label onClick={()=>handclick(options[2])} for={options[2]}> <input type="radio" id={options[2]} name="contact" value="email" checked />{options[2]}</label>
         </div>
 
        <div className='bg-black text-white p-5 text-center flex items-center'>
-             
+       <ToastContainer />
              <label onClick={()=>handclick(options[3])} for={options[3]}><input type="radio" id={options[3]} name="contact" value="email" checked />{options[3]}</label>
        </div>
 </from>
+        </div>
         </div>
     );
 };
